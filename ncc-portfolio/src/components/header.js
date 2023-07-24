94c0deb95fbc7405351ee611d62365f02685064c
calasCosta <noamicafft@gmail.com>
2023-07-24 03:27:24 +0000

    My firt commit of ncc-portfolio

---
+++ ncc-portfolio/src/components/header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function header() {
+  return (
+    <div>
+      <h1>Logo</h1>
+      <nav>
+        <a href="/">Home</a>
+        <a href="/about">About</a>
+        <a href="/contact">Contact</a>
+      </nav>
+    </div>
+  )
+}

7f29bbe76c2458939f3dadb94d847fe16769d9f2
calasCosta <noamicafft@gmail.com>
2023-07-26 21:24:27 +0000

    terminate header pc screen

--- ncc-portfolio/src/components/header.js
+++ ncc-portfolio/src/components/header.js
@@ -3,12 +3,14 @@ import React from 'react'
 export default function header() {
   return (
     <header>
-      <h1>Logo</h1>
-      <nav>
-        <a href="/">Home</a>
-        <a href="/about">About</a>
-        <a href="/contact">Contact</a>
-      </nav>
+      <div className='headerDiv'>
+        <h1>Logo</h1>
+        <nav>
+          <a href="/">Home</a>
+          <a href="/about">About</a>
+          <a href="/contact">Contact</a>
+        </nav>
+      </div>
     </header>
   )
 }

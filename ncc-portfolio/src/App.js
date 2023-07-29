6fdb7732fa9d1451245ad6a4618b9ba962b11322
calasCosta <noamicafft@gmail.com>
2023-07-29 07:29:38 +0000

    Last commit, skeleton for 7/29/2023

--- ncc-portfolio/src/App.js
+++ ncc-portfolio/src/App.js
@@ -5,6 +5,7 @@ import HeroArea from './components/HeroArea';
 import Experiences from './components/Experiences';
 import Projects from './components/Projects';
 import Certificates from './components/Certificates';
+import Skills from './components/Skills';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Header />
       <HeroArea />
       <Experiences />
+      <Skills />
       <Projects />
       <Certificates />
     </div>

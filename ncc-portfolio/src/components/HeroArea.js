d0f925bdfd1df5cf4d461fcf1da76a06d26154f9
calasCosta <noamicafft@gmail.com>
2023-07-29 01:42:51 +0000

    continue hero section

--- ncc-portfolio/src/components/HeroArea.js
+++ ncc-portfolio/src/components/HeroArea.js
@@ -11,7 +11,7 @@ export default function HeroArea() {
                 <img className='profile-photo' src={profile} alt='hero-img' />
             </div>
             <div className='right'>
-                <h2 className='h1-main'> Hello, my name is Noami Costa </h2>
+                <h3 className='h1-main'> Hello, my name is Noami Costa </h3>
                 <h1 className='h2-main'> I'm a software engineer </h1>
                 <h3 className='h3-main'> I enjoy to code and learning new things </h3>   
                 <h4 className='h4-main'> Feel free to contact me </h4>

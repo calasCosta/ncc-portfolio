aa662dde8d0738a6b14fed7eb8f164f3abd1fd91
calasCosta <noamicafft@gmail.com>
2023-08-20 19:43:53 +0000

    add projects

--- ncc-portfolio/src/components/Projects.js
+++ ncc-portfolio/src/components/Projects.js
@@ -4,6 +4,37 @@ export default function Projects() {
   return (
     <section>
       <h2>Projects</h2>
+
+      <div>
+        <p> <span>Web Programming</span>  – The main objective of this project was to develop a Full-Stack
+            web solution that would allow the creation of a website with information about 
+            video games and a forum on the subject, using HTML, CSS, JavaScript, Node.js/Express.js and MySQL.
+        </p>
+
+        <p> <span>Project of the Database Complements Course</span> which aims to familiarize with the administration of relational 
+            databases using SQLServer, through the implementation of a new system for grouping schools named "Agrupamento STB". <br/>
+            One of the measures adopted by the group is to develop a new information system that allows it to manage, in an integrated way,
+            the entire process of launching grades of the different schools in the grouping, as well as allowing students and guardians to
+            have access to this information. <br/>
+            With the implementation of the new system, it became necessary to model and integrate, in a new Database, the pieces of information
+            that were exported from existing systems and that up to now supported the various schools in the cluster. The main concepts used
+            were Programming (Stored Procedures, Functions, Triggers), Metadata, Indexes, Backup and Recovery, Access levels, Encryption, and
+            transaction control.
+        </p>
+
+        <p> <span>Java Advanced Programming</span>  - A logistics network consists of the location of the distribution centers and product movements between
+            these centers when each route is bidirectional. This program consists of a graphical application in which the user views, manipulates, 
+            and obtains diverse information about a logistics network. <br/>
+            Throughout the project, the following several abstract data types were used: ArrayList, HashMap, LinkedHashMap. We also used data types
+            by us implemented as following shown: Graph, GraphAdjacencyList, Vertex and Edge. Referring to Design Patterns, we used: Observer Pattern,
+            Strategy Pattern, MVC Pattern. We also used refactoring techniques to avoid bad smells in the program.
+        </p>
+        
+        <p> <span>Mobile Computing </span>  - This project aimed to create a mobile app to help prevent forgetting disease (Alzheimer) through a memory match game. 
+                  Two implementations were made for this application, Android Studio (Java) and React Native versions.
+        </p>
+
+      </div>
     </section>
   )
 }
--- ncc-portfolio/src/components/Skills.js
+++ ncc-portfolio/src/components/Skills.js
@@ -1,12 +1,44 @@
-import React from 'react'
+import React from 'react';
+import javaIcon from '../images/javaIcon.png';
+import javascriptIcon from '../images/javascriptIcon.png';
+import pyIcon from '../images/pythonIcon.png';
+import msqlServerIcon from '../images/msqlServerIcon.png';
+import mysqlIcon from '../images/mysqlIcon.png';
+import htmlIcon from '../images/htmlIcon.png';
+import cssIcon from '../images/cssIcon.png';
+import reactIcon from '../images/reactIcon.png';
+import ejsIcon from '../images/ejsIcon.png';
+import expressIcon from '../images/express-jsIcon.png';
+import gitIcon from '../images/gitIcon.png';
+import githubIcon from '../images/githubIcon.png';
+
+function Skill(props){
+  return (
+    <div style={{textAlign: "center", display:"inline-block", margin:"4%"}}>
+      <img src={props.img} alt={props.name} className="skill-icon" />
+      <p className={props.className}> {props.name} </p>
+    </div>
+  );
+}
 
 export default function Skills() {
   return (
     <section>
       <h2>Skills</h2>
-
-      <div>
-        
+      <div style={{textAlign: 'center', width: "35%"}}>
+        <Skill  className="skill" name="Java" img={javaIcon} />
+        <Skill  className="skill" name="JavaScript" img={javascriptIcon} />
+        <Skill  className="skill" name="HTML" img={htmlIcon} />
+        <Skill  className="skill" name="CSS" img={cssIcon} />
+        <Skill  className="skill" name="React" img={reactIcon} />
+        <Skill  className="skill" name="Python" img={pyIcon} />
+        <Skill  className="skill" name="MSQLServer" img={msqlServerIcon} />
+        <Skill  className="skill" name="MySQL" img={mysqlIcon} />
+        <Skill  className="skill" name="EJS" img={ejsIcon} />
+        <Skill  className="skill" name="Express.js" img={expressIcon} />
+        <Skill  className="skill" name="Git" img={gitIcon} />
+        <Skill  className="skill" name="GitHub" img={githubIcon} />
+        <Skill  className="skill" name="JDBC" img={javaIcon} />
       </div>
     </section>
   )

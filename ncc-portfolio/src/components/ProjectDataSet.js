4e85562772bbac24a0d465452911725213915f27
calasCosta <noamicafft@gmail.com>
2023-08-22 17:12:34 +0000

    improve projects section

---
+++ ncc-portfolio/src/components/ProjectDataSet.js
@@ -0,0 +1,47 @@
+
+
+const dataSet = [
+    {
+        title : "Web Programming",
+        description : `The main objective of this project was to develop a Full-Stack
+        web solution that would allow the creation of a website with information about 
+        video games and a forum on the subject, using HTML, CSS, JavaScript, Node.js/Express.js and MySQL.`
+    },
+    {
+        title: "Database Complements Course",
+        description: `which aims to familiarize with the administration of relational 
+        databases using SQLServer, through the implementation of a new system for grouping schools named "Agrupamento STB".
+        One of the measures adopted by the group is to develop a new information system that allows it to manage, in an integrated way,
+        the entire process of launching grades of the different schools in the grouping, as well as allowing students and guardians to
+        have access to this information.
+        With the implementation of the new system, it became necessary to model and integrate, in a new Database, the pieces of information
+        that were exported from existing systems and that up to now supported the various schools in the cluster. The main concepts used
+        were Programming (Stored Procedures, Functions, Triggers), Metadata, Indexes, Backup and Recovery, Access levels, Encryption, and
+        transaction control.`,
+    },
+    {
+        title: "Java Advanced Programming",
+        description: `A logistics network consists of the location of the distribution centers and product movements between
+        these centers when each route is bidirectional. This program consists of a graphical application in which the user views, manipulates, 
+        and obtains diverse information about a logistics network.
+        Throughout the project, the following several abstract data types were used: ArrayList, HashMap, LinkedHashMap. We also used data types
+        by us implemented as following shown: Graph, GraphAdjacencyList, Vertex and Edge. Referring to Design Patterns, we used: Observer Pattern,
+        Strategy Pattern, MVC Pattern. We also used refactoring techniques to avoid bad smells in the program.`,
+    },
+    {
+        title: "Mobile Computing",
+        description: `This project aimed to create a mobile app to help prevent forgetting disease (Alzheimer) through a memory match game. 
+        Two implementations were made for this application, Android Studio (Java) and React Native versions.`,
+    },
+    {
+        title: "Cucumber + Selenium + Java + Maven",
+        description: `the objective of this task was to acquire knowledge concerning the performance of automated tests of 
+        web applications, through the search and implementation of solutions for several possible scenarios while using a website.`,
+    },
+    {
+        title: "EngWithYTV (Final Bachelor Project)",
+        description: `This project aims to create a fullstack web application that allows the user to study English through YouTube videos.`,
+    }
+];
+
+export default dataSet;
--- ncc-portfolio/src/components/Projects.js
+++ ncc-portfolio/src/components/Projects.js
@@ -1,43 +1,40 @@
 import React from 'react'
+import dataSet from './ProjectDataSet';
+
+
+function Project(props){
+  const description = props.description;
+  return(
+    <div className="project">
+      <h4>{props.title}</h4>
+      { (description.length > 200) ?
+        <p> {description.slice(0,200)}...</p> :
+        <p>{description}</p>
+      }
+      {
+        (description.length > 200) &&
+          <p className="seeMore">see more</p>
+      }
+      
+    </div>
+  );
+}
+
 
 export default function Projects() {
+
+  const ds = dataSet;
   return (
     <section>
       <h2>Projects</h2>
 
-      <div >
-        <p> <span>Web Programming</span>  – The main objective of this project was to develop a Full-Stack
-            web solution that would allow the creation of a website with information about 
-            video games and a forum on the subject, using HTML, CSS, JavaScript, Node.js/Express.js and MySQL.
-        </p>
-
-        <p> <span>Project of the Database Complements Course</span> which aims to familiarize with the administration of relational 
-            databases using SQLServer, through the implementation of a new system for grouping schools named "Agrupamento STB". <br/>
-            One of the measures adopted by the group is to develop a new information system that allows it to manage, in an integrated way,
-            the entire process of launching grades of the different schools in the grouping, as well as allowing students and guardians to
-            have access to this information. <br/>
-            With the implementation of the new system, it became necessary to model and integrate, in a new Database, the pieces of information
-            that were exported from existing systems and that up to now supported the various schools in the cluster. The main concepts used
-            were Programming (Stored Procedures, Functions, Triggers), Metadata, Indexes, Backup and Recovery, Access levels, Encryption, and
-            transaction control.
-        </p>
-
-        <p> <span>Java Advanced Programming</span>  - A logistics network consists of the location of the distribution centers and product movements between
-            these centers when each route is bidirectional. This program consists of a graphical application in which the user views, manipulates, 
-            and obtains diverse information about a logistics network. <br/>
-            Throughout the project, the following several abstract data types were used: ArrayList, HashMap, LinkedHashMap. We also used data types
-            by us implemented as following shown: Graph, GraphAdjacencyList, Vertex and Edge. Referring to Design Patterns, we used: Observer Pattern,
-            Strategy Pattern, MVC Pattern. We also used refactoring techniques to avoid bad smells in the program.
-        </p>
-        
-        <p> <span>Mobile Computing </span>  - This project aimed to create a mobile app to help prevent forgetting disease (Alzheimer) through a memory match game. 
-                  Two implementations were made for this application, Android Studio (Java) and React Native versions.</p>
-
-        <p> <span>Cucumber + Selenium + Java + Maven </span> - the objective of this task was to acquire knowledge concerning the performance of automated tests of 
-                  web applications, through the search and implementation of solutions for several possible scenarios while using a website. </p>
-
-        <p> <span>EngWithYTV (Final Bachelor Project)</span> - This project aims to create a fullstack web application that allows the user to study English through YouTube videos.</p>
-
+      <div className="projectsDiv">
+      {ds.map((p)=>
+          <Project 
+                title={p.title} 
+                description={p.description}
+          /> 
+      )}      
       </div>
     </section>
   )

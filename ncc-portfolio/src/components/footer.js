f212cc197b2762ba0eeccb67a79fd3f70da79263
calasCosta <noamicafft@gmail.com>
2023-08-24 02:09:22 +0000

    create responsive heroarea

--- ncc-portfolio/src/components/footer.js
+++ ncc-portfolio/src/components/footer.js
@@ -1,10 +1,14 @@
-import React from 'react'
+import React from 'react';
+
 
 export default function Footer() {
   return (
     <footer>
-      <p>All right reserved</p>
-      <p>Create by Noami Costa (using react.js) </p>
+
+      <div>
+        <p>All right reserved</p>
+        <p>Created with ❤️ by Noami Costa (using react.js) </p>
+      </div>
     </footer>
   )
 }

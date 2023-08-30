e224e41a95819b241be4a30c87aa7c2f5a5d26cb
calasCosta <noamicafft@gmail.com>
2023-08-30 04:30:32 +0000

    navigate(-1) to go to the previous page

--- ncc-portfolio/src/pages/Project.js
+++ ncc-portfolio/src/pages/Project.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import Footer from '../components/Footer';
 import dataSet from '../components/ProjectDataSet';
+import { useNavigate } from 'react-router-dom';
 
 export default function Project() {
   const pathname = window.location.hash;
   const index = pathname.split("/")[2];
   const projectObj = dataSet[index];
 
+  const navigate = useNavigate();
   const goBack = () => {
-    window.location.href = window.location.origin;
+    navigate(-1);
   }
 
   return (
